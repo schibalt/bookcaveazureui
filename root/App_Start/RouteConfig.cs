e879feeab1b5f1df70e70a0ad0646d2b3339a8bc
andrew <art26@zips.uakron.edu>
2013-06-17 19:02:35 -0400

    readded old project

---
+++ root/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace root
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Book",
+                url: "book/{isbn13}",
+                defaults: new { controller = "Book", action = "Details" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+        }
+    }
+}
